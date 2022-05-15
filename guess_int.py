28d82edc41c6daf2fff07171549227cdb1cf1ebf
cs15510 <98731161+cs15510@users.noreply.github.com>
2022-05-15 09:13:43 +0800

    add range
    add range function

--- guess_int.py
+++ guess_int.py
@@ -1,9 +1,10 @@
 import random
 
-number = random.randint(1, 100)
+guess_rang = int(input("請輸入你想猜的範圍: "))
+number = random.randint(1, guess_rang)
 count = 0
 while True:
-	guess_int = int(input("請輸入數字(1~100): "))
+	guess_int = int(input("請輸入數字(1~"+str(guess_rang)+"): "))
 	count += 1
 	if guess_int == number:
 		print("終於猜對了, 這是你猜的第", count, "次")
